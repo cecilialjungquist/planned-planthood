b2a3a604d1c2f5d297b1c8654e090ca999eea741
cecilialjungquist <111697322+cecilialjungquist@users.noreply.github.com>
2023-04-07 10:19:38 +0200

    Updated input to React standards

--- src/Components/AddPlant.jsx
+++ src/Components/AddPlant.jsx
@@ -1,18 +1,25 @@
 import Input from "./Input";
 import './AddPlant.css';
 import Button from "./Button";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { PlantingsContext } from "../App";
 
 function AddPlant() {
     const { allPlantings, setAllPlantings } = useContext(PlantingsContext);
-    let newPlant = {};
+    const [newPlant, setNewPlant] = useState({
+        seed: '',
+        date: '',
+        image: '',
+        responsible: ''
+    });
 
     function handleChange(event) {
-        newPlant = {
-            ...newPlant,
-            [event.target.name]: event.target.value
-        }
+        setNewPlant(prevPlant => {
+            return {
+                ...prevPlant,
+                [event.target.name]: event.target.value
+            }
+        })
     }
 
     function addPlant(e) {
@@ -21,6 +28,15 @@ function AddPlant() {
         if (newPlant.seed && newPlant.date && newPlant.image && newPlant.responsible) {
             setAllPlantings(prevPlantings => [...prevPlantings, newPlant]);
         }
+
+        setNewPlant(prevPlant => {
+            return {
+                seed: '',
+                date: '',
+                image: '',
+                responsible: ''
+            }
+        })
     }
 
 
@@ -30,10 +46,10 @@ function AddPlant() {
             <form>
                 <h3>Ny sådd:</h3>
                 <section className="input__section">
-                    <Input type={'text'} label={'Frönamn'} placeholder={'Tomat'} name={'seed'} action={handleChange} />
-                    <Input type={'date'} label={'Datum för sådd'} name={'date'} action={handleChange} />
-                    <Input type={'url'} label={'URL, exempelbild'} placeholder={'https://dinbild.com'} name={'image'} action={handleChange} />
-                    <Input type={'text'} label={'Planterare'} placeholder={'Emma på 4an'} name={'responsible'} action={handleChange} />
+                    <Input type={'text'} label={'Frönamn'} placeholder={'Tomat'} name={'seed'} action={handleChange} value={newPlant.seed} />
+                    <Input type={'date'} label={'Datum för sådd'} name={'date'} action={handleChange} value={newPlant.date} />
+                    <Input type={'url'} label={'URL, exempelbild'} placeholder={'https://dinbild.com'} name={'image'} action={handleChange} value={newPlant.image} />
+                    <Input type={'text'} label={'Planterare'} placeholder={'Emma på 4an'} name={'responsible'} action={handleChange} value={newPlant.responsible} />
                 </section>
                 <Button type={'add'} text={'Lägg till'} action={addPlant}/>
             </form>
--- src/Components/Input.jsx
+++ src/Components/Input.jsx
@@ -1,11 +1,11 @@
 import './Input.css';
 
-function Input({ type, label, placeholder, name, action }) {
-
+function Input({ type, label, placeholder, name, value, action }) {
+    
     return ( 
         <section className="input">
             <label>{label}</label>
-            <input type={type} placeholder={placeholder} name={name} onChange={(event) => action(event)}/>
+            <input type={type} placeholder={placeholder} name={name} value={value} onChange={(event) => action(event)}/>
         </section>
     );
 }
